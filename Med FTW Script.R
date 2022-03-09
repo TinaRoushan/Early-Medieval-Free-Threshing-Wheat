20d93206ac2d37d68f150af65083a3b4792b3beb
TinaRoushan <tina_roushan@live.co.uk>
2022-03-09 11:46:42 +0000

    xxx

--- Med FTW Script.R
+++ Med FTW Script.R
@@ -7,3 +7,5 @@ FTWRefCoords <- list.files("D:\\Coordinates\\Free Threshing\\FeedSax_Ref", full.
 FTWRefFrame <- read.csv("D:\\Coordinate_Frames\\Charred_Frame_FeedSax_Ref.csv", header = TRUE)
 FTWRefTxt <- import_txt(FTWRefCoords, fileEncoding="UTF-8-BOM")#fileencoding gets rid of weird symbols
 FTWRefOut <- Out(FTWRefTxt, fac=FTWRefFrame) #creates an Out object from a specified list- you can specify landmarks in "ldk"
+FTWRefOut2 <-coo_scale (FTWRefOut)
+#comment
