ce73933c7b0e6ef6945ca95048e995457e9cb68a
TinaRoushan <tina_roushan@live.co.uk>
2022-03-09 12:20:09 +0000

    xxx

--- Med FTW Script.R
+++ Med FTW Script.R
@@ -8,7 +8,4 @@ FTWRefFrame <- read.csv("D:\\Coordinate_Frames\\Charred_Frame_FeedSax_Ref.csv", 
 FTWRefTxt <- import_txt(FTWRefCoords, fileEncoding="UTF-8-BOM")#fileencoding gets rid of weird symbols
 FTWRefOut <- Out(FTWRefTxt, fac=FTWRefFrame) #creates an Out object from a specified list- you can specify landmarks in "ldk"
 FTWRefOut2 <-coo_scale (FTWRefOut)
-#comment2222
-#comment
-#comment3
-############
+###
