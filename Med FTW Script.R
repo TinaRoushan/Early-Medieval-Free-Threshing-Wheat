fb32fd56a8c043359bf94432978b0d2a91181cc7
TinaRoushan <tina_roushan@live.co.uk>
2022-03-09 11:30:31 +0000

    Testing

---
+++ Med FTW Script.R
@@ -0,0 +1,4 @@
+packageurl <- "https://cran.r-project.org/src/contrib/Archive/Momocs//Momocs_1.2.9.tar.gz"
+install.packages(packageurl, repos=NULL, type="source")
+packageVersion('Momocs')
+library('Momocs')
