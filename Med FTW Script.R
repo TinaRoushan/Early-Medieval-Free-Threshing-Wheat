340fc7f21a0d6d3cfb3df60d26c2d428f4d6a851
TinaRoushan <tina_roushan@live.co.uk>
2022-06-10 12:53:41 +0100

    Added method test and accession LDA

--- Med FTW Script.R
+++ Med FTW Script.R
@@ -1,5 +1,57 @@
   library('Momocs')
-  library('MASS')
+  library(geomorph)
+ #Repeatability and measurement error
+  MTFTWCoords <- list.files("D:\\Coordinates\\Free Threshing\\FTW _Method_Test", full.names = TRUE)
+  MTFTWFrame <- read.csv("D:\\Coordinate_Frames\\MethodTest_FTW.csv", header = TRUE)
+  MTFTWTxt <- import_txt(MTFTWCoords, fileEncoding="UTF-8-BOM")#fileencoding gets rid of weird symbols
+  MTFTWOut <- Out(MTFTWTxt, fac=MTFTWFrame) #creates an Out object from a specified list- you can specify landmarks in "ldk"
+  MTFTWOut.l <- filter(MTFTWOut, View == "l")#creates subset of lateral views only
+  MTFTWOut.d <- filter(MTFTWOut, View == "d")#creates subset of lateral views only
+  MTFTWOut.l.efour <- efourier(MTFTWOut.l, nb.h=8, norm = FALSE, start = FALSE)
+  MTFTWOut.d.efour <- efourier(MTFTWOut.d, nb.h=8, norm = FALSE, start = FALSE)
+  MTFTWOut.d.l <- combine (MTFTWOut.d, MTFTWOut.l)
+  #Procrustes ANOVA
+  export(MTFTWOut.l.efour)
+  export(MTFTWOut.d.efour)
+  MTFTW_l <- read.csv("D:\\Coordinate_Frames\\MTFTW_l.csv")
+  MTFTW_d <- read.csv("D:\\Coordinate_Frames\\MTFTW_d.csv")
+  #Calculate PCA scores for above
+  PCAObservation_d<-prcomp(MTFTW_d)$x 
+  PCAObservation_l<-prcomp(MTFTW_l)$x 
+  #Creates 5 level session factor (consecutive)
+  sessionfactor<-as.factor(gl(5, 5))
+  sessionfactor
+  #Create 5 level individual factor (repeated)
+  individualfactor<-as.factor(rep(1:5, 5))
+  individualfactor
+  #Geomorph data frames for PCA computations of coefficients
+  gdf_d<-geomorph.data.frame(shape=PCAObservation_d) #ID: a vector containing the specimens ID 
+  gdf_l<-geomorph.data.frame(shape=PCAObservation_l) #ID: a vector containing the specimens ID 
+  #Procrustes ANOVA using session factor and then individual factor as dependent variables
+  summary(procD.lm(shape~sessionfactor, data=gdf_d))
+  summary(procD.lm(shape~sessionfactor, data=gdf_l))
+  #=session difference not significant for dorsal or lateral view
+  mod<-summary(procD.lm(shape~individualfactor, data= gdf_d))
+  mod2<-summary(procD.lm(shape~individualfactor, data= gdf_l))
+  mod
+  mod2
+  #=indiv difference significant for dorsal and lateral view
+  #dorsal measurement error (See Claude 2008)
+  s2within<-mswithin<-mod[[1]][2,3]
+  mod[[1]][2, 3]
+  MSamong<-mod[[1]][1, 3]
+  MSamong
+  s2among<-(MSamong-mswithin)/5
+  s2within/(s2within+s2among)*100#measurement error
+  #lateral measurement error
+  s2within<-mswithin<-mod2[[1]][2,3]
+  mod2[[1]][2, 3]
+  MSamong<-mod2[[1]][1, 3]
+  MSamong
+  s2among<-(MSamong-mswithin)/5
+  s2within/(s2within+s2among)*100#measurement error
+  
+  
   #Stage 1 Uncharred
   FTWRefCoords <- list.files("D:\\Coordinates\\Free Threshing\\Uncharred.01", full.names = TRUE)
   FTWRefCoords
@@ -24,7 +76,8 @@
   FTWRefOut.p %>% chop(~View) %>% lapply(efourier,nb.h=8, norm = FALSE, start = FALSE) %>% combine %>% LDA ('taxon.code', scale=FALSE, center= TRUE) 
   FTWRefOut.d.l %>% chop(~View) %>% lapply(efourier,nb.h=8, norm = FALSE, start = FALSE) %>% combine %>% LDA ('taxon.code', scale=FALSE, center= TRUE) 
   FTWRefOut.d.l.p %>% chop(~View) %>% lapply(efourier,nb.h=8, norm = FALSE, start = FALSE) %>% combine %>% LDA ('taxon.code', scale=FALSE, center= TRUE) 
-#Charred reference material 
+
+  #Charred reference material 
   FTWRefCoords <- list.files("D:\\Coordinates\\Free Threshing\\Charred.02", full.names = TRUE)
   FTWRefCoords
   FTWRefFrame <- read.csv("D:\\Coordinate_Frames\\Charred_Frame_FT.csv", header = TRUE)
@@ -89,9 +142,10 @@
   #LDA
   #LDA 
   FTWRefOut.d %>% chop(~View) %>% lapply(efourier,nb.h=8, norm = FALSE, start = FALSE) %>% combine %>% LDA ('taxon.code', scale=FALSE, center= TRUE) 
-  FTWRefOut.l %>% chop(~View) %>% lapply(efourier,nb.h=8, norm = FALSE, start = FALSE) %>% combine %>% LDA ('taxon.code', scale=FALSE, center= TRUE) 
+  FTWRefOut.l %>% chop(~View) %>% lapply(efourier,nb.h=8, norm = FALSE, start = FALSE) %>% combine %>% LDA ('taxon.code', scale=FALSE, center= TRUE)
   FTWRefOut.d.l %>% chop(~View) %>% lapply(efourier,nb.h=8, norm = FALSE, start = FALSE) %>% combine %>% LDA ('taxon.code', scale=FALSE, center= TRUE) 
-  FTWRefOut.d.l.lda <- FTWRefOut.d.l %>% chop(~View) %>% lapply(efourier,nb.h=8, norm = FALSE, start = FALSE, center= TRUE) %>% combine %>% LDA ('taxon.code', scale=FALSE, center= TRUE)
+  FTWRefOut.d.l %>% chop(~View) %>% lapply(efourier,nb.h=8, norm = FALSE, start = FALSE) %>% combine %>% LDA ('Accession', scale=FALSE, center= TRUE) 
+   FTWRefOut.d.l.lda <- FTWRefOut.d.l %>% chop(~View) %>% lapply(efourier,nb.h=8, norm = FALSE, start = FALSE, center= TRUE) %>% combine %>% LDA ('taxon.code', scale=FALSE, center= TRUE)
   FTWRefOut.d.l.lda
   plot(FTWRefOut.d.l.lda, points= TRUE , labelspoints= FALSE, zoom= 1.4, cex.labelsgroups= 0.8, rect.labelsgroups= TRUE, cex = 0.1)
   
@@ -123,7 +177,8 @@
   FTWRefOutG.d.l %>% chop(~View) %>% lapply(efourier,nb.h=8, norm = FALSE, start = FALSE) %>% combine %>% LDA ('taxon.code', scale=FALSE, center= TRUE) 
   FTWRefOutG.d.l.lda <- FTWRefOutG.d.l %>% chop(~View) %>% lapply(efourier,nb.h=8, norm = FALSE, start = FALSE, center= TRUE) %>% combine %>% LDA ('taxon.code', scale=FALSE, center= TRUE, prior= c(1,1,1,1,1)/5)
   plot(FTWRefOutG.d.l.lda, points= TRUE , labelspoints= FALSE, zoom= 1.2, cex.labelsgroups= 0.8, rect.labelsgroups= TRUE, cex = 0.1)
-   #Comparison with arch
+
+  #Comparison with arch
  #Unknown LDA reclass
   UnclassCoords <- list.files("D:\\Coordinates\\Free Threshing\\Unclassified", full.names = TRUE)
   UnclassFrame <- read.csv("D:\\Coordinate_Frames\\Unclassified3.csv", header = TRUE)
